bb4b01ae599386ab4079462438986f2bd3d92fea
PhumlaniKubeka <52313491+PhumlaniKubeka@users.noreply.github.com>
2019-09-06 16:45:14 +0200

    Add files via upload

--- password.py
+++ password.py
@@ -4,53 +4,56 @@ def password_is_valid(password):
     val = True
     if len(password) < 8: 
         raise Exception('length should be at least 8') 
-        val = False
+        val = True
 
-    if password == None: 
+    if len(password) == 0: 
         raise Exception('Password should not be empty') 
-        val = False
+        val = True
 
     if not any(char.isupper() for char in password): 
         raise Exception('Password should have at least one uppercase letter') 
-        val = False
+        val = True
 
     if not any(char.islower() for char in password): 
         raise Exception('Password should have at least one lowercase letter') 
-        val = False
+        val = True
+
     if not any(char.isdigit() for char in password): 
         raise Exception('Password should have at least one numeral') 
-        val = False   
+        val = True
+          
     if val: 
         return val 
         
 def password_is_ok(password):
     count = 0
-    while(count < 0):
-        val = True
-        if len(password) < 8: 
-            raise Exception('length should be at least 8') 
-            count += 1
-            val = False
-            return val
+    if count < 3:
+            val = True
+            if len(password) < 8: 
+                raise Exception('length should be at least 8') 
+                count += 1
+                val = False
 
-        if password == None: 
-            raise Exception('Password should not be empty') 
-            count += 1
-            val = False
+            if password == None: 
+                raise Exception('Password should not be empty') 
+                count += 1
+                val = False
 
-        if not any(char.isupper() for char in password): 
-            raise Exception('Password should have at least one uppercase letter') 
-            count += 1
-            val = False
+            if not any(char.isupper() for char in password): 
+                raise Exception('Password should have at least one uppercase letter') 
+                count += 1
+                val = False
 
-        if not any(char.islower() for char in password): 
-            raise Exception('Password should have at least one lowercase letter') 
-            count += 1
-            val = False
-        if not any(char.isdigit() for char in password): 
-            raise Exception('Password should have at least one numeral') 
-            count += 1
-            val = False   
-        if val: 
-            return val 
-            
+            if not any(char.islower() for char in password): 
+                raise Exception('Password should have at least one lowercase letter') 
+                count += 1
+                val = False
+            if not any(char.isdigit() for char in password): 
+                raise Exception('Password should have at least one numeral') 
+                count += 1
+                val = False   
+            if val: 
+                return val 
+    else:
+         raise Exception('Your Password is not valid') 
+                
--- test_password.py
+++ test_password.py
@@ -1,10 +1,18 @@
 from password import password_is_valid, password_is_ok
+def test_pass_larger_than_eight():
+        assert password_is_valid('2ujdhjhjhRjkt') == True
 
+def test_lower_case_letter():
+        assert password_is_valid('ESKSKKKDKdF') == True
 
-def test_password_is_valid():
-  
-        assert password_is_valid('Tdhjgg1fhj') == True
+def test_upper_case_letter():
+        assert password_is_valid('sfdkgkhdlgk') == True
+
+def  test_atleast_one_number():
+        assert password_is_valid('KDSLKFLKDSLKiL1') == True
+
+def test_is_null():
+        assert password_is_valid("") == False
+
 def test_password_is_ok():
     assert password_is_ok("sfdjsDsas1") == True
-        
-   
